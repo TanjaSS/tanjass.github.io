6bf3cfa3ec6d262f93e8f863b6cc06b918bc6de5
Tanja Stjepanović Stričević <stricevictanja@gmail.com>
2015-07-14 00:00:51 +0200

    Combine CSS

---
+++ Gruntfile.js
@@ -0,0 +1,41 @@
+module.exports = function(grunt) {
+  require('jit-grunt')(grunt);
+  
+  grunt.loadNpmTasks('grunt-contrib-cssmin');
+  grunt.loadNpmTasks('grunt-contrib-uglify');
+
+  grunt.initConfig({
+    less: {
+      development: {
+        options: {
+          compress: false
+        },
+        files: {
+          'css/style.css': 'less/style.less' // destination file and source file
+        }
+      }
+    },
+    watch: {
+      styles: {
+        files: ['less/**/*.less'], // which files to watch
+        tasks: ['less'],
+        options: {
+          nospawn: true
+        }
+      }
+    },
+    cssmin: {
+      target: {
+        files: {
+          'css/style.min.css': [
+            'bower_components/bootstrap/dist/css/bootstrap.min.css',
+            'vendor/slick/slick.css',
+            'css/style.css'
+          ]
+        }
+      }
+    }
+  });
+
+  grunt.registerTask('default', ['less', 'cssmin', 'watch']);
+}
